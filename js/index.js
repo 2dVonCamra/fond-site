8ce12e05ebf9f51f73ec9b9d8b7f7dc9365af759
2dVonCamra <camra279@gmail.com>
2026-07-10 19:48:51 +0300

    Чистовая версия без конфликтов

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const burger = document.getElementById('burger');
 const sideMenu = document.getElementById('sideMenu');
 const overlay = document.getElementById('overlay');
@@ -70,76 +70,3 @@ window.addEventListener('load', function () {
         preloader.classList.add('preloader--hidden');
     }
 });
-=======
-const burger = document.getElementById('burger');
-const sideMenu = document.getElementById('sideMenu');
-const overlay = document.getElementById('overlay');
-
-
-burger.addEventListener('click', () => {
-    burger.classList.toggle('active');
-    const isOpen = sideMenu.classList.toggle('active');
-    overlay.classList.toggle('active');
-    burger.setAttribute('aria-expanded', isOpen);
-});
-
-overlay.addEventListener('click', () => {
-    burger.classList.remove('active');
-    sideMenu.classList.remove('active');
-    overlay.classList.remove('active');
-    burger.setAttribute('aria-expanded', 'false');
-
-});
-
-document.addEventListener('keydown', (e) => {
-    if (e.key === 'Escape' && sideMenu.classList.contains('active')) {
-        burger.classList.remove('active');
-        sideMenu.classList.remove('active');
-        overlay.classList.remove('active');
-        burger.setAttribute('aria-expanded', 'false');
-        burger.blur();
-    }
-});
-
-
-const scrollToTopBtn = document.getElementById('scrollToTopBtn');
-
-window.addEventListener('scroll', function () {
-
-    if (window.scrollY > 300) {
-        scrollToTopBtn.classList.add('visible');
-    } else {
-        scrollToTopBtn.classList.remove('visible');
-    }
-
-
-    let scrollPoint = 350;
-
-    if (window.innerWidth <= 576) {
-        scrollPoint = 150;
-    } else if (window.innerWidth <= 1023) {
-        scrollPoint = 100;
-    }
-
-
-    if (window.scrollY > scrollPoint) {
-        document.body.classList.add('scrolled');
-    } else {
-        document.body.classList.remove('scrolled');
-    }
-});
-
-scrollToTopBtn.addEventListener('click', function () {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-});
-
-window.addEventListener('load', function () {
-    const preloader = document.getElementById('preloader');
-    if (preloader) {
-        preloader.classList.add('preloader--hidden');
-    }
-});
->>>>>>> 9a947a4187919816fe3d22ffb219c04f3c8bafb6
